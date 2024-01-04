73de886f61a8aa2918845f6a3266896c9e789220
harikaran044 <153819367+harikaran044@users.noreply.github.com>
2024-01-04 15:32:26 +0530

    Add files via upload

---
+++ Markting/src/pages/Login.jsx
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Footer, Navbar } from "../components";
+
+const Login = () => {
+  const [uname, setUname] = useState("");
+  const [pname, setPname] = useState("");
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setUname(e.target.value);
+    setPname(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+if(uname&&pname)
+{
+  alert("success");
+  navigate('/');
+
+}
+else{
+  alert("failed");
+}
+
+
+    
+  };
+
+  return (
+    <>
+      <Navbar />
+      <div className="container my-3 py-3">
+        <h1 className="text-center">Login</h1>
+        <hr />
+        <div className="row my-4 h-100">
+          <div className="col-md-4 col-lg-4 col-sm-8 mx-auto">
+            <form onSubmit={handleSubmit}>
+              <div className="my-3">
+                <label htmlFor="floatingInput">Email address</label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="floatingInput"
+                  placeholder="name@example.com"
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="my-3">
+                <label htmlFor="floatingPassword">Password</label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="floatingPassword"
+                  placeholder="Password"
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="my-3">
+                <p>
+                  New Here?{" "}
+                  <Link to="/register" className="text-decoration-underline text-info">
+                    Register
+                  </Link>{" "}
+                </p>
+              </div>
+              <div className="text-center">
+                <button className="my-2 mx-auto btn btn-dark" type="submit">
+                  Login
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default Login;
